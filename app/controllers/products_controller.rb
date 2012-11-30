65fa8c219320e37e9cf639c95eb03362e8e938d2
Mark Ellis-Jones <markellisjones@gmail.com>
2012-11-30 07:03:41 +0700

    first commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,17 @@
+class ProductsController < ApplicationController
+  def index
+  @products = Product.all
+  end
+
+
+  def new
+  end
+
+    def show
+  	@product = Product.find(params[:id])
+  end
+
+
+  def edit
+  end
+end
